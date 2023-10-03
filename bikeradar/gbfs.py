3ad789778bc21fbd72a3071caced30933d554688
Øyvind Evju <oyvevju@gmail.com>
2023-10-03 20:21:16 +0200

    Add station status to marker popup

--- bikeradar/gbfs.py
+++ bikeradar/gbfs.py
@@ -11,8 +11,26 @@ class Station(BaseModel):
     capacity: int
 
 
+class StationStatus(BaseModel):
+    station_id: str
+    num_bikes_available: int
+    num_docks_available: int
+    is_installed: int
+    is_renting: int
+    is_returning: int
+    last_reported: int
+
+
 async def get_stations() -> list[Station]:
     response = requests.get(
         "https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json"
     )
     return [Station.model_validate(station) for station in response.json()["data"]["stations"]]
+
+
+async def get_status() -> dict[str, StationStatus]:
+    response = requests.get("https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json")
+    return {
+        station["station_id"]: StationStatus.model_validate(station)
+        for station in response.json()["data"]["stations"]
+    }
--- bikeradar/main.py
+++ bikeradar/main.py
@@ -2,7 +2,15 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from folium import Map, Marker
 
-from bikeradar.gbfs import get_stations
+from bikeradar.gbfs import Station, StationStatus, get_stations, get_status
+
+
+def popup(station: Station, station_status: StationStatus) -> str:
+    return f"""
+    <b>{station.name}</b><br>
+    Ledige sykler: {station_status.num_bikes_available}<br>
+    Ledige plasser: {station_status.num_docks_available}
+    """
 
 
 def run():
@@ -13,9 +21,13 @@ def run():
         m = Map()
 
         stations = await get_stations()
+        station_status = await get_status()
 
         for station in stations:
-            marker = Marker(location=[station.lat, station.lon], popup=station.name)
+            marker = Marker(
+                location=[station.lat, station.lon],
+                popup=popup(station, station_status[station.station_id]),
+            )
             marker.add_to(m)
 
         return m._repr_html_()
