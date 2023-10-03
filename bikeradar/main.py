f7ce65fbb6e87891e433dce791ae125868237046
Øyvind Evju <oyvevju@gmail.com>
2023-10-03 20:12:04 +0200

    Add markers for all stations

--- bikeradar/main.py
+++ bikeradar/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-from folium import Map
+from folium import Map, Marker
+
+from bikeradar.gbfs import get_stations
 
 
 def run():
@@ -9,6 +11,13 @@ def run():
     @app.get("/", response_class=HTMLResponse)
     async def root():
         m = Map()
+
+        stations = await get_stations()
+
+        for station in stations["data"]["stations"]:
+            marker = Marker(location=[station["lat"], station["lon"]], popup=station["name"])
+            marker.add_to(m)
+
         return m._repr_html_()
 
     return app
