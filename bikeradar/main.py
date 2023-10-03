d80f07d4a128ef8ac278cef789ecaee2cd2621d4
Øyvind Evju <oyvevju@gmail.com>
2023-10-03 22:08:50 +0200

    Add proper icon, with color corresponding to number of available bikes

--- bikeradar/main.py
+++ bikeradar/main.py
@@ -1,6 +1,8 @@
+from datetime import datetime
+
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-from folium import Map, Marker
+from folium import Icon, Map, Marker
 
 from bikeradar.gbfs import GBFSApi, Station, StationStatus
 
@@ -13,6 +15,17 @@ def popup(station: Station, station_status: StationStatus) -> str:
     """
 
 
+def icon_color(station_status: StationStatus):
+    # TODO: Deal with potential timezone issues
+    if datetime.now().timestamp() - station_status.last_reported > 5 * 60:
+        return "gray"
+    if station_status.num_bikes_available == 0:
+        return "red"
+    if station_status.num_bikes_available < 3:
+        return "orange"
+    return "green"
+
+
 def run():
     gbfs_api = GBFSApi("https://gbfs.urbansharing.com/oslobysykkel.no/gbfs.json", "nb")
     app = FastAPI()
@@ -23,11 +36,17 @@ def run():
 
         stations = await gbfs_api.get_stations()
         station_status = await gbfs_api.get_status()
+        m.fit_bounds([(station.lat, station.lon) for station in stations])
 
         for station in stations:
             marker = Marker(
                 location=[station.lat, station.lon],
                 popup=popup(station, station_status[station.station_id]),
+                icon=Icon(
+                    color=icon_color(station_status[station.station_id]),
+                    icon="bicycle",
+                    prefix="fa",
+                ),
             )
             marker.add_to(m)
 
